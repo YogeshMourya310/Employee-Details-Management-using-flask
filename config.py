081028df3501d31201083a95a40842ba23c929d2
YogeshMourya310 <112762036+YogeshMourya310@users.noreply.github.com>
2024-09-03 19:32:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Yogesh%40123@localhost:3306/pythondb1'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:{{secrets.sql_password}}@localhost:3306/pythondb1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
